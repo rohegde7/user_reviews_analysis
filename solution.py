c0da660bb3d8ae660cc3fae9dffff654431d1547
rohegde7 <hegde.rohit7@gmail.com>
2018-03-06 14:23:03 +0530

    bar graph drawn for +ve/-ve keywords with diff colours

--- solution.py
+++ solution.py
@@ -1,3 +1,10 @@
+'''
+status:
+    -categorised all the +ve & -ve reviews for speed
+    -next: draw a bar graph for all review keywords for speed
+    -next: do the same for price factor
+'''
+
 import pandas as pd
 
 xl_DataFrame = pd.read_excel('external-hard-disk-drive.xlsx')    #stores in the form of a DataFrame object
@@ -14,7 +21,6 @@ for i in range(no_records):
     list_reviews.append(xl_DataFrame.iloc[i, 1])
     # 1 because we need the 2nd column review only
     # i represents the 'i'th row/record
-
 #print(list_reviews)    #working
 
 list_speed = ['speed', 'clock']
@@ -22,10 +28,11 @@ list_positive_rev_for_speed = ['best', 'lovely', 'great', 'fast', 'good']
 list_negative_for_speed = ['disappointed', 'utterly', 'hate', 'worst', 'worthless', 'cheated', 'waste', 'bad']
 dict_no_negative_reviews_for_speed = {}
 dict_no_positive_reviews_for_speed = {}
-positive_revs = 0 #initial positive and negative reviews
-negative_revs = 0
+count_positive_revs_for_speed = 0 #initial positive and negative reviews
+count_negative_revs_for_speed = 0
 
 '''
+                    SPEED!
     1st: check for speed keywords in the list of reviews
     2nd: if found, check for positive/negative keywords in the review list
 '''
@@ -34,7 +41,7 @@ for rev in list_reviews:
         if speed in rev.lower():
             for pos in list_positive_rev_for_speed:
                 if pos in rev.lower():
-                    positive_revs += 1
+                    count_positive_revs_for_speed += 1
 
                     if pos in dict_no_positive_reviews_for_speed:
                         dict_no_positive_reviews_for_speed[pos] += 1
@@ -43,45 +50,54 @@ for rev in list_reviews:
             
             for neg in list_negative_for_speed:
                 if neg in rev.lower():
-                    negative_revs += 1
+                    count_negative_revs_for_speed += 1
 
                     if neg in dict_no_negative_reviews_for_speed:
                         dict_no_negative_reviews_for_speed[neg] += 1
                     else:
                         dict_no_negative_reviews_for_speed[neg] = 1
                     
-print(dict_no_positive_reviews_for_speed)
-print(dict_no_negative_reviews_for_speed)
+#print(dict_no_positive_reviews_for_speed)
+#print(dict_no_negative_reviews_for_speed)
 
-#print("pos:", positive_revs, "neg:", negative_revs)
-#working but cannot confirm the accuracy
+#print("pos:", count_positive_revs_for_speed, "neg:", count_negative_revs_for_speed)
 
 '''
-Up next: Plotting the bar graph for +ve and -ve revs
+Up next: Plotting the bar graph for +ve reviews of SPEED
 '''
 
 import matplotlib.pyplot as plt
 
-x_coordinates = [1,2]
-list_height = [positive_revs, negative_revs]
+total_x_coor = len(dict_no_positive_reviews_for_speed) + len(dict_no_negative_reviews_for_speed)
+x = range(total_x_coor)
+#how many +ve distinct keywords are present, to be on x-axis
 
-bar_labels = ['+ve', '-ve']
+#y = [count_positive_revs_for_speed, count_negative_revs_for_speed]
+#bar_labels = ['+ve', '-ve']
 
-plt.bar(x_coordinates, list_height, tick_label = bar_labels, width = 0.8, color = ['green', 'red'])
+y_pos = []
+bar_labels_pos = []
+for i in dict_no_positive_reviews_for_speed:
+    bar_labels_pos.append(i)
+    y_pos.append(dict_no_positive_reviews_for_speed[i])
+
+
+y_neg = []
+bar_labels_neg = []
+for i in dict_no_negative_reviews_for_speed:
+    bar_labels_neg.append(i)
+    y_neg.append(dict_no_negative_reviews_for_speed[i])
+
+y = y_pos + y_neg
+bar_labels = bar_labels_pos + bar_labels_neg
+
+#y = y_pos
+#bar_labels = bar_labels_pos
+
+color = ['green']*len(dict_no_positive_reviews_for_speed) + ['red']*len(dict_no_negative_reviews_for_speed)
+
+plt.bar(x, y, tick_label = bar_labels, width = 0.4, color = color)
 
 plt.title('Hard-Disk review for SPEED')
 plt.show()
 
-'''
-Next: plotting bar graph for number of times different review words have occured
-'''
-
-x_coordinates = [1,2]
-list_height = [positive_revs, negative_revs]
-
-bar_labels = ['+ve', '-ve']
-
-plt.bar(x_coordinates, list_height, tick_label = bar_labels, width = 0.8, color = ['green', 'red'])
-
-plt.title('Review words frequency')
-plt.show()
