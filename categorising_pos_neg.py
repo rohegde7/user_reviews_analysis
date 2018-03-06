f3284c9bc798cf9731ea8082f8cff78bee29a09d
rohegde7 <hegde.rohit7@gmail.com>
2018-03-06 13:02:23 +0530

    speed- all +ve/-ve keywords separated, next:bar graph

--- categorising_pos_neg.py
+++ categorising_pos_neg.py
@@ -17,10 +17,11 @@ for i in range(no_records):
 
 #print(list_reviews)    #working
 
-list_speed = ['speed']
-list_positive = ['best', 'lovely', 'great', 'fast', 'good']
-list_negative = ['disappointed', 'utterly', 'hate', 'worst', 'worthless', 'cheated', 'waste']
-
+list_speed = ['speed', 'clock']
+list_positive_rev_for_speed = ['best', 'lovely', 'great', 'fast', 'good']
+list_negative_for_speed = ['disappointed', 'utterly', 'hate', 'worst', 'worthless', 'cheated', 'waste', 'bad']
+dict_no_negative_reviews_for_speed = {}
+dict_no_positive_reviews_for_speed = {}
 positive_revs = 0 #initial positive and negative reviews
 negative_revs = 0
 
@@ -31,19 +32,32 @@ negative_revs = 0
 for rev in list_reviews:
     for speed in list_speed:
         if speed in rev.lower():
-            for pos in list_positive:
+            for pos in list_positive_rev_for_speed:
                 if pos in rev.lower():
                     positive_revs += 1
+
+                    if pos in dict_no_positive_reviews_for_speed:
+                        dict_no_positive_reviews_for_speed[pos] += 1
+                    else:
+                        dict_no_positive_reviews_for_speed[pos] = 1
             
-            for neg in list_negative:
+            for neg in list_negative_for_speed:
                 if neg in rev.lower():
                     negative_revs += 1
+
+                    if neg in dict_no_negative_reviews_for_speed:
+                        dict_no_negative_reviews_for_speed[neg] += 1
+                    else:
+                        dict_no_negative_reviews_for_speed[neg] = 1
+                    
+print(dict_no_positive_reviews_for_speed)
+print(dict_no_negative_reviews_for_speed)
 
 #print("pos:", positive_revs, "neg:", negative_revs)
 #working but cannot confirm the accuracy
 
 '''
-For plotting the graphs: visit plotting_graph.py
+Up next: Plotting the bar graph for +ve and -ve revs
 '''
 
 import matplotlib.pyplot as plt
@@ -55,5 +69,19 @@ bar_labels = ['+ve', '-ve']
 
 plt.bar(x_coordinates, list_height, tick_label = bar_labels, width = 0.8, color = ['green', 'red'])
 
-plt.title('Hard-Disk review')
+plt.title('Hard-Disk review for SPEED')
+plt.show()
+
+'''
+Next: plotting bar graph for number of times different review words have occured
+'''
+
+x_coordinates = [1,2]
+list_height = [positive_revs, negative_revs]
+
+bar_labels = ['+ve', '-ve']
+
+plt.bar(x_coordinates, list_height, tick_label = bar_labels, width = 0.8, color = ['green', 'red'])
+
+plt.title('Review words frequency')
 plt.show()
