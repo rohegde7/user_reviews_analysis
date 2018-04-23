bef5a47979f28aa6bea45dcf753b94904f8709e4
rohegde7 <hegde.rohit7@gmail.com>
2018-04-23 22:48:29 +0530

    Testing basic features

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-lst = ['aBcde']
+import re
+string1 = "4-98 20/20 results269 should get"
 
-x = 'bc'
+list_usb_speed_numbers = list(map(str, re.findall(r'(\d+)', str(string1))))
 
-if 'bc' in lst[0].lower():
-    print("yes")
+
+x = 'abcd4'
+print(x.lower)
